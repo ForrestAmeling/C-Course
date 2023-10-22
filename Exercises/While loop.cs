57fecebbea305bad4950cffc43a97fc1ba0516f7
GumpGaming <anxin4392@gmail.com>
2023-10-22 14:45:36 -0400

    //While loop part 2

--- Exercises/While loop.cs
+++ Exercises/While loop.cs
@@ -1,10 +1,26 @@
-﻿//While loop
-var number = 0;
-while(number < 10)
+﻿//While loop part 1
+//var number = 0;
+//while(number < 10)
+//{
+//    number++;
+//    Console.WriteLine($"Number is: {number}");
+//}
+//Console.WriteLine("The loop is finished!");
+
+
+//While loop part 2
+Console.WriteLine("Enter a word");
+var userInput = Console.ReadLine();
+
+while (userInput).Length < 15)
+
 {
-    number++;
-    Console.WriteLine($"Number is: {number}");
+    userInput += 'a';
+    //userInput = userInput + 'a';
+
+    Console.WriteLine(userInput);
 }
-Console.WriteLine("The loop is finished!");
+Console.WriteLine(userInput"The loop is finished");
+Console.ReadKey();
 
 
