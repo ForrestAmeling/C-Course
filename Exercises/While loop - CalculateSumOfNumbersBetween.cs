296338c575d96458248d92c0cb7853eb783f943b
GumpGaming <anxin4392@gmail.com>
2023-10-22 15:09:04 -0400

    While loop - CalculateSumOfNumbersBetween

---
+++ Exercises/While loop - CalculateSumOfNumbersBetween.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+        {
+            int sum = 0;
+            while (firstNumber <= lastNumber)
+            {
+                sum += firstNumber;
+                firstNumber++;
+            }
+            return sum;
+        }
+    }
+}
