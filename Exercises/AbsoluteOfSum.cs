68f37a1153a0353a3f44f3075e54629540c03dd0
GumpGaming <anxin4392@gmail.com>
2023-10-22 10:49:02 -0400

    AbsoluteOfSum and Parse

---
+++ Exercises/AbsoluteOfSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int AbsoluteOfSum(int a, int b)
+        {
+            return Math.Abs(a + b);
+        }
+    }
+}
+
+
+//Other way to soleve this exercise
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int AbsoluteOfSum(int a, int b)
+        {
+            var sum = a + b;
+            if(sum>=0)
+            {
+                return sum;
+            }
+            return -sum;
+        }
+    }
+}
---
+++ Exercises/ParseStringToInt.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Provide a number:");
+sting userInput = Console.ReadLine();
+int numberr= int.Parse(userInput);
+Console.WriteLine(numberr); 
+
+
