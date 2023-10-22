f7c3f6e098069710910e452aa65b04e359691f75
GumpGaming <anxin4392@gmail.com>
2023-10-22 13:57:12 -0400

    switch statement

---
+++ Exercises/Switch.cs
@@ -0,0 +1,24 @@
+﻿string ConvertPointsToGrade(int points)
+{
+    switch (points)
+    {
+        case 10:
+        case 9:
+            result = "A";
+        case 8:
+        case 7:
+        case 6:
+            result = "B";
+        case 5:
+        case 4:
+        case 3:
+            result = "C";
+        case 2:
+        case 1:
+            resut = "D";
+        case 0:
+            result = "F";
+        default:
+            result = "Invalid grade";
+    }
+}
